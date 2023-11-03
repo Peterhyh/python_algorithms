2af2288a793d0929ecba68f4ffb3543cfa2eea0a
Peter Huynh <peterhyh@yahoo.com>
2023-11-02 20:49:51 -0500

    quick sort clean up

--- quick_sort.py
+++ quick_sort.py
@@ -1,22 +1,21 @@
-#Divide and conquer algorithm
-#Make the pivot the last index number
-#Create 2 variables: i and j
-#i will iterate from left to right to find the numbers bigger than pivot
-#j will iterate from right to left to find the numbers smaller than the pivot
-#Once j passes i, swap i with pivot
+# Divide and conquer algorithm
+# Make the pivot the last index number
+# Create 2 variables: i and j
+# i will iterate from left to right to find the numbers bigger than pivot
+# j will iterate from right to left to find the numbers smaller than the pivot
+# Once j passes i, swap i with pivot
 
 
 def quicksort(arr, left, right):
     if left < right:
         partition_pos = partition(arr, left, right)
-        quicksort(arr, left, partition_pos - 1)                 #calling quicksort on all elements that are less then the pivot element
-        quicksort(arr, partition_pos + 1, right) 
+        # calling quicksort on all elements that are less then the pivot element
+        quicksort(arr, left, partition_pos - 1)
+        quicksort(arr, partition_pos + 1, right)
 
 
-
-
-
-def partition(arr, left, right):                                #returns the index of the pivot element after the first step of quicksort
+# returns the index of the pivot element after the first step of quicksort
+def partition(arr, left, right):
     i = left
     j = right - 1
     pivot = arr[right]
@@ -34,6 +33,7 @@ def partition(arr, left, right):                                #returns the ind
         arr[i], arr[right] = arr[right], arr[i]
 
     return i
+
 
 arr = [22, 11, 88, 66, 55, 77, 33, 44]
 
