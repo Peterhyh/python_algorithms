c672f77621301d72c26e7ebe36c2afc686be89dc
Peter Huynh <peterhyh@yahoo.com>
2023-11-04 14:58:43 -0500

    insertion sort clean up

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,10 +1,9 @@
-#Comparing numbers side by side from left to right and switching their index as needed
+# Comparing numbers side by side from left to right and switching their index as needed
 
 array_list = [2, 6, 5, 3, 7]
 
-
 for i in range(1, len(array_list)):
-    j = i 
+    j = i
     while array_list[j - 1] > array_list[j] and j > 0:
         array_list[j - 1], array_list[j] = array_list[j], array_list[j - 1]
         j -= 1
